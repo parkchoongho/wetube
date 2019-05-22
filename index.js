69722555bd3353250f2eb09c5298d3142bdb8556
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-05-22 23:57:28 +0900

    Making Express Server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hi');
+import express from "express"; //import express
+const app = express(); // call express
+
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile.");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
