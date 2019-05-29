a516088de2266608e3698533af9fc2e752864121
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-05-29 23:58:02 +0900

    View Part

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleWare } from "./middlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 
+
 const app = express(); // call express
 
+app.use(helmet());
 app.set("view engine", "pug");
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleWare)
 
 app.use(routes.home, globalRouter); // 전역적 Router
 app.use(routes.users, userRouter);
