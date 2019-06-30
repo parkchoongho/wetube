684c27bea33aafbe725dbcc45ea9d7e92ad88a27
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-06-30 16:04:06 +0900

    View Part1

--- app.js
+++ app.js
@@ -9,17 +9,15 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
 
-
 const app = express(); // call express
 
 app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-
-app.use(localsMiddleWare)
+app.use(localsMiddleWare);
 
 app.use(routes.home, globalRouter); // 전역적 Router
 app.use(routes.users, userRouter);
