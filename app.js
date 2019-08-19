70fda022228442ca9304af698eff024d4695b298
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-08-19 17:06:38 +0900

    Backend of RegisterView

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import routes from "./routes";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 import "./passport";
 
 const app = express(); // call express
@@ -41,5 +42,6 @@ app.use(localsMiddleWare);
 app.use(routes.home, globalRouter); // 전역적 Router
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use(routes.api, apiRouter);
 
 export default app;
--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,16 +3,27 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  console.log(event.data);
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start Recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
   videoRecorder.addEventListener("dataavailable", handleVideoData);
-  // 데이터는 레코딩이 다 끝나야 가질 수 있음
+  // 데이터는 레코딩이 다 끝나야 가질 수 있음 왜냐하면 MediaRecorder가 파일 전체를 한번에 저장하기 때문이다.
+  // 따라서 videoRecorder.start안에 1000이라는 인수를 넣어 1초마다 정보를 가지고 올 수 있게 한다.
+  // 작은 패킷 및 데이터를 가져가고 싶을 때 이러한 방법을 사용하면 된다. (비디오의 일부분을 record하고 싶을 때)
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
