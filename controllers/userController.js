a1441bd02465069dad38c7bfc9df553028e10be3
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-02 16:17:01 +0900

    add controllers

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send("JOIN");
+export const login = (req, res) => res.send("LOGIN");
+export const logout = (req, res) => res.send("LOGOUT");
+
+export const users = (req, res) => res.send("USERS");
+export const userDetail = (req, res) => res.send("USER DETAIL");
+export const editProfile = (req, res) => res.send("EDIT PROFILE");
+export const changePassword = (req, res) => res.send("CHANGE PASSWORD");
