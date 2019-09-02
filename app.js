bb45b003f8198f851dce936b97122c04e8dfb094
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-02 16:22:39 +0900

    update app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
@@ -11,5 +15,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
