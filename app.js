627772b39b72376e51c76296689e1083208f0732
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-02 14:21:54 +0900

    make express server

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+export default app;
