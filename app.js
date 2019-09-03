5ccf4e9525add8119986a21670b59fb1f4206d76
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-03 13:01:59 +0900

    set view engine to pug and add localMiddleware

--- app.js
+++ app.js
@@ -4,17 +4,20 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes";
+import { localMiddleware } from "./localMiddlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
