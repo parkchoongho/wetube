c2ad40a2df194d32a6d5d3ce42a2f71c70d96154
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-03 23:26:40 +0900

    modify router files

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,8 +1,8 @@
 import express from "express";
 import routes from "../routes";
 import {
-  vidoes,
-  upload,
+  getUpload,
+  postUpload,
   videoDetail,
   editVideo,
   deleteVideo
@@ -10,9 +10,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, vidoes);
-videoRouter.get(routes.upload, upload);
-videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.upload, getUpload);
+videoRouter.post(routes.upload, postUpload);
+
+videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
