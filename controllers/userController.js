1d3018ea9410ed6a6f1ac03a143c846e0b21f802
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-03 12:59:25 +0900

    update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,12 @@
-export const join = (req, res) => res.send("JOIN");
-export const login = (req, res) => res.send("LOGIN");
-export const logout = (req, res) => res.send("LOGOUT");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "Logout" });
 
-export const users = (req, res) => res.send("USERS");
-export const userDetail = (req, res) => res.send("USER DETAIL");
-export const editProfile = (req, res) => res.send("EDIT PROFILE");
-export const changePassword = (req, res) => res.send("CHANGE PASSWORD");
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "Change Password" });
