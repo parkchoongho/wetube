7d5ffa7de0427e5ef4d7c576d6e2ccf261174d77
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-11 21:42:31 +0900

    add session & session storage & passport setting

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import helmet from "helmet";
 import morgan from "morgan";
 import routes from "./routes";
@@ -9,7 +13,11 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
+import "./passport";
+
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -19,6 +27,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
