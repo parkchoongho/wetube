de3729a3fa63330f5cad4accfa372673711af0c5
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-11 21:40:19 +0900

    add onlyPublic & onlyPrivate to router files

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -8,18 +8,19 @@ import {
   postLogin,
   logout
 } from "../controllers/userController";
+import { onlyPublic, onlyPrivate } from "../localMiddlewares";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
 export default globalRouter;
