765b207850a699698e15eb32d6ba644dea11fd6d
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-12 13:33:51 +0900

    add GithubStrategy in passport.js

--- passport.js
+++ passport.js
@@ -1,7 +1,20 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
 import User from "./models/User";
+import routes from "./routes";
+import { githubLoginCallback } from "./controllers/userController";
 
 passport.use(User.createStrategy());
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: `http://localhost:${process.env.PORT}${routes.githubCallback}`
+    },
+    githubLoginCallback
+  )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
