cd9198935426e5918ce223be2b07540a4345a897
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-16 21:46:27 +0900

    add post route for changePassword url

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,9 +2,10 @@ import express from "express";
 import routes from "../routes";
 import {
   userDetail,
-  changePassword,
   getEditProfile,
-  postEditProfile
+  postEditProfile,
+  getChangePassword,
+  postChangePasssword
 } from "../controllers/userController";
 import { onlyPrivate, avatarMulter } from "../localMiddlewares";
 
@@ -13,7 +14,9 @@ const userRouter = express.Router();
 userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, avatarMulter, postEditProfile);
 
-userRouter.get(routes.changePassword, onlyPrivate, changePassword);
+userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePasssword);
+
 userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
