1b387c806605818d21d7156536fc60ee6c8837e1
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-16 21:48:41 +0900

    revise the wrong alphabet

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,7 +5,7 @@ import {
   getEditProfile,
   postEditProfile,
   getChangePassword,
-  postChangePasssword
+  postChangePassword
 } from "../controllers/userController";
 import { onlyPrivate, avatarMulter } from "../localMiddlewares";
 
@@ -15,7 +15,7 @@ userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
 userRouter.post(routes.editProfile, onlyPrivate, avatarMulter, postEditProfile);
 
 userRouter.get(routes.changePassword, onlyPrivate, getChangePassword);
-userRouter.post(routes.changePassword, onlyPrivate, postChangePasssword);
+userRouter.post(routes.changePassword, onlyPrivate, postChangePassword);
 
 userRouter.get(routes.userDetail(), userDetail);
 
