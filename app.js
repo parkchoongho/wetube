5977a28248bc5822b948187e8c8f533db462b160
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-22 12:08:29 +0900

    make route for apiRouter.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ import { localMiddleware } from "./localMiddlewares";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import apiRouter from "./routers/apiRouter";
 
 import "./passport";
 
@@ -43,5 +44,6 @@ app.use(localMiddleware);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
+app.use(routes.api, apiRouter);
 
 export default app;
