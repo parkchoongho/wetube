114dbf8797a66b9e170ac9dd1ef6885b0b5cfd4d
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-23 20:26:56 +0900

    add addComment route

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import routes from "../routes";
-import { registerView } from "../controllers/videoController";
+import { registerView, addComment } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 apiRouter.post(routes.registerView, registerView);
+apiRouter.post(routes.addComment, addComment);
 
 export default apiRouter;
