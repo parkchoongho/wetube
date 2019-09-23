05f304efffa59d925d4a5c853be26c83bef6b7c8
parkchoongho <34790763+parkchoongho@users.noreply.github.com>
2019-09-23 20:25:19 +0900

    add comment api route

--- routes.js
+++ routes.js
@@ -26,6 +26,7 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 // Register View API
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -66,7 +67,8 @@ const routes = {
   githubCallback: GITHUB_CALLBACK,
   me: ME,
   api: API,
-  registerView: REGISTER_VIEW
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
